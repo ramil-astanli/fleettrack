00e3e1a2e2fe2f55c22d0ea7a65eca9773f7a913
ramil-astanli <ramilastanli19@gmail.com>
2026-05-26 12:17:47 +0400

    feat: initial project setup

---
+++ src/main/java/com/fleettrack/FleettrackApplication.java
@@ -0,0 +1,19 @@
+package com.fleettrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableCaching
+@EnableScheduling
+@EnableAsync
+public class FleettrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleettrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fleettrack/FleettrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleettrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleettrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
